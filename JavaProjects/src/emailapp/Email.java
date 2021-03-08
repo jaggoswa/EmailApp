f48abde55880bc12522fdfbc1df63126c597cff3
jaggoswa <jaggoswa@192.168.0.183>
2021-03-09 02:59:12 +0530

    Initial Setup

---
+++ JavaProjects/src/emailapp/Email.java
@@ -0,0 +1,24 @@
+package emailapp;
+
+public class Email {
+	private String firstname;
+	private String lastname;
+	private String password;
+	private String department;
+	private int mailboxCapacity;
+	private String alternateEmail;
+	
+	//Constructor to receive the first and last name
+	public Email(String firstName, String lastName){
+		this.firstname = firstName;
+		this.lastname = lastName;
+	}
+	
+	//Ask for the department
+	
+	//Generate a random password
+	
+	//Set the mailbox capacity
+	
+	//Set the alternate email
+}
